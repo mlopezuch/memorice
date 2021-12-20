2c410867ab0175b32300a48fdf952dd4e7d4fffe
mlopezuch <matilopezuch@gmail.com>
2021-12-20 03:51:30 -0300

    Mas cambios

---
+++ src/SingleCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SingleCard() {
+    return (
+        <div className='card'>
+            <img className='figure' src="/img/potion.png" alt="figure" />
+            <img className='cover' src="/img/cover.png" alt="cover" />
+        </div>
+    )
+}
+
+export default SingleCard
